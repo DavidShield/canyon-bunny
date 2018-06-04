e2d266bd183392d43f76b99b0802ff413ce1dcda
DavidShield <your_emailwdw19930307@gmail.com>
2018-06-04 11:04:42 -0400

    first commit

---
+++ core/src/com/mygdx/game/Test.java
@@ -0,0 +1,93 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+
+public class Test extends ApplicationAdapter {
+//	SpriteBatch batch;
+//	Texture img;
+
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private Texture texture;
+    private Sprite sprite;
+    private float rot;
+
+    private WorldController worldController;
+    private WorldRenderer worldRenderer;
+
+
+    private boolean paused;
+	@Override
+	public void create () {
+		//run when app is created
+
+//		batch = new SpriteBatch();
+//		img = new Texture("badlogic.jpg");
+
+        // set libgdx log level to debug
+        // need to change to log_none or log_info before publishing game
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
+        // initialize controller and renderer
+        worldController = new WorldController();
+        worldRenderer = new WorldRenderer(worldController);
+
+        // game is active on start
+        paused = false;
+	}
+
+	@Override
+	public void render () {
+		//run as app is running
+
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
+
+        // do not update the app while paused
+        if(!paused) {
+            worldController.update(Gdx.graphics.getDeltaTime());
+        }
+
+        // Sets the clear screen color to: Cornflower Blue
+        Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        worldRenderer.render();
+	}
+
+	
+	@Override
+	public void dispose () {
+//		batch.dispose();
+//		texture.dispose();
+
+        worldRenderer.render();
+	}
+
+	@Override
+    public void resize(int width, int height) {
+        worldRenderer.resize(width, height);
+    }
+
+    @Override
+    public void pause() {
+        paused = true;
+    }
+
+    @Override
+    public void resume() {
+        paused = false;
+    }
+}
---
+++ core/src/com/mygdx/game/util/Constants.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.util;
+
+/**
+ * Created by shield on 5/31/18.
+ * Class to define the constants in the project
+ */
+
+
+
+public class Constants {
+    //visiable game world is 5 * 5 meters.
+    public static final float VIEWPORT_WIDTH = 5.0f;
+    public static final float VIEWPORT_HEIGHT = 5.0f;
+
+}
